082987641363c081d06af2efa757462ec29013d8
Nader Tak <nadertak7@gmail.com>
2025-12-10 23:43:12 +0000

    Add new glyphs to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <FastLED.h>
+
 #include <PixelMapper.h>
+#include <GlyphManager.h>
+#include <DrawingEngine.h>
 
 const uint8_t PIN_BUZZER = 15;
 const uint8_t PIN_BUTTON_LEFT = 26;
@@ -16,6 +19,8 @@ CRGB leds[NUM_LEDS];
 
 // Create library class instances
 PixelMapper pixelMapper(LED_PIXEL_WIDTH, LED_PIXEL_HEIGHT);
+DrawingEngine drawingEngine(leds, &pixelMapper);
+
 
 void setup() {
     // Pin modes from https://github.com/rroels/ulanzi_tc001_hardware
@@ -24,21 +29,16 @@ void setup() {
     pinMode(PIN_BUTTON_MIDDLE, INPUT_PULLUP);
     pinMode(PIN_BUTTON_RIGHT, INPUT_PULLUP);
 
+    initialseAsciiCharacterTable();
+
     // Link LED array with ulanzi hardware
     FastLED.addLeds<NEOPIXEL, PIN_LED_MATRIX>(leds, NUM_LEDS);
-
-    // Example: Set pixel at position (0, 0) to white using PixelMapper
-    int index = pixelMapper.xyToIndex(0, 0);
-    if (index >= 0) {
-        leds[index] = CRGB(255, 0, 255);
-    }
-
-    FastLED.setBrightness(1);
-    FastLED.show();
 }
 
 void loop() {
-    if(digitalRead(PIN_BUTTON_LEFT) == LOW) {
-        Serial.println("left button");
-    }
+    drawingEngine.drawGlyph(asciiCharacterTable['A'], 1, 1, CRGB(255, 0, 255));
+    drawingEngine.drawGlyph(getCustomGlyph("heart"), 6, 1, CRGB(0, 255, 255));
+
+    FastLED.setBrightness(1);
+    FastLED.show();
 }
