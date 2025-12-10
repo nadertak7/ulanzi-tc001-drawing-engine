61f9c741932e08ed9a55ed4eabc74516c11fdb27
Nader Tak <nadertak7@gmail.com>
2025-12-10 23:42:27 +0000

    Improve redability of drawGlyph by iterating on bitshift

--- lib/DrawingEngine/DrawingEngine.cpp
+++ lib/DrawingEngine/DrawingEngine.cpp
@@ -12,26 +12,28 @@ void DrawingEngine::drawPixel(int x, int y, CRGB colour) {
 
 void DrawingEngine::drawGlyph(const Glyph* glyph, int x, int y, CRGB colour) {
     for(int row = 0; row < glyph->height; row++) {
-        // Retrieve bitmap for row n
+
+        uint8_t bitshift = glyph->width - 1;
         uint8_t charRowBits = glyph->bitmap[row];
 
         for(int col = 0; col < glyph->width; col++) {
-            // We bitshift on 1 n times based on column index and use an AND mask on the row bitmap.
+            // We bitshift on 1 n times for column index and use an AND mask on the row bitmap.
             // When passed into a bool, a binary literal with 1+ set bit returns True,
             // and a binary literal with no set bits returns False.
 
-            // e.g 2nd column (col index 1) on a shape of width of 5 where row is
-            // 0b11011
-            // Would result in
+            // e.g 2nd column (col index 1) on a shape of width of 5 on row bitmap 0b11011
+            // Would result in:
             // ledOn = 0b11011 & (1 << 5 - 1 - 1)
             // ledOn = 0b11011 % (1 << 3)
             // ledOn = 0b11011 & 0b01000
             // ledOn = 0b01000 (non-zero value, so truthy)
-            bool ledOn = charRowBits & (1 << (glyph->width - 1 - col));
+            bool ledOn = charRowBits & (1 << bitshift);
 
             if(ledOn) {
                 drawPixel(x + col, y + row, colour);
             }
+
+            bitshift--;
         }
     }
 }
