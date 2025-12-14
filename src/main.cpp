5be201963c38f68b3c85b97022c4b6ff9294a668
Nader Tak <nadertak7@gmail.com>
2025-12-14 00:54:14 +0000

    Move constants to include dir

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,7 @@
 #include <PixelMapper.h>
 #include <GlyphManager.h>
 #include <DrawingEngine.h>
-
-const uint8_t PIN_BUZZER = 15;
-const uint8_t PIN_BUTTON_LEFT = 26;
-const uint8_t PIN_BUTTON_MIDDLE = 27;
-const uint8_t PIN_BUTTON_RIGHT = 14;
-const uint8_t PIN_LED_MATRIX = 32;
-const uint8_t LED_PIXEL_WIDTH = 32;
-const uint8_t LED_PIXEL_HEIGHT = 8;
-const uint16_t NUM_LEDS = LED_PIXEL_WIDTH * LED_PIXEL_HEIGHT;
+#include <Config.h>
 
 // Create an array that allows assigning RGB values to each index
 CRGB leds[NUM_LEDS];
